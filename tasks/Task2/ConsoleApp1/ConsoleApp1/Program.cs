9ce23f804930d05d006bb929b17439014e551c14
Aksit <oezguer.aksit@atos.net>
2018-03-06 10:45:28 +0100

    add project Task2

---
+++ tasks/Task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car redCar = new Car("Max Mustermann");
+            redCar.licensePlate = "W - 123456";
+            redCar.vinNumber = "WVWZZZ6NZTY112309";
+            Console.WriteLine("red-car initial:\n" + redCar.getShortSummary() + "\n");
+            redCar.updateOwnerId("Hans Meier");
+            Console.WriteLine("red-car altered:\n" + redCar.getShortSummary() + "\n");
+
+            Car blueCar = new Car("Max Mustermann's Frau");
+            blueCar.licensePlate = "W - 123457";
+            blueCar.vinNumber = "WVWZZZ6NZTY112310";
+            Console.WriteLine("blue-car initial:\n" + blueCar.getShortSummary() + "\n");
+            blueCar.updateOwnerId("Hans Meier's Frau");
+            Console.WriteLine("blue-car altered:\n" + blueCar.getShortSummary() + "\n");
+        }
+    }
+
+    class Car
+    {
+        // 1 private field
+        private String ownerId;
+
+        // 2 public properties (auto-implemented property)
+        public String vinNumber { get; set; }
+        public String licensePlate { get; set; }
+
+        // 1 constructor
+        public Car(String owner)
+        {
+            this.ownerId = owner;
+        }
+
+        // 1 or more public method(s)
+        public String getShortSummary()
+        {
+            return "Owner: " + this.ownerId + "\nVIN: " + this.vinNumber + "\nLicense plate: " + this.licensePlate;
+        }
+
+        public void updateOwnerId(String newOwner)
+        {
+            this.ownerId = newOwner;
+        }
+
+    }
+}
