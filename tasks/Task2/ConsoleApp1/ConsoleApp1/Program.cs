9befca3aa3eea218e24ec186ec94acd79916c919
Aksit <oezguer.aksit@atos.net>
2018-03-13 16:21:25 +0100

    modified project Task2 according to lesson3

--- tasks/Task2/ConsoleApp1/ConsoleApp1/Program.cs
+++ tasks/Task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,46 +10,135 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Car redCar = new Car("Max Mustermann");
-            redCar.licensePlate = "W - 123456";
-            redCar.vinNumber = "WVWZZZ6NZTY112309";
-            Console.WriteLine("red-car initial:\n" + redCar.getShortSummary() + "\n");
-            redCar.updateOwnerId("Hans Meier");
-            Console.WriteLine("red-car altered:\n" + redCar.getShortSummary() + "\n");
+            Sedan sedan1 = new Sedan();
+            Console.WriteLine("sedan1 initial:\n" + sedan1.getShortSummary() + "\n");
 
-            Car blueCar = new Car("Max Mustermann's Frau");
-            blueCar.licensePlate = "W - 123457";
-            blueCar.vinNumber = "WVWZZZ6NZTY112310";
-            Console.WriteLine("blue-car initial:\n" + blueCar.getShortSummary() + "\n");
-            blueCar.updateOwnerId("Hans Meier's Frau");
-            Console.WriteLine("blue-car altered:\n" + blueCar.getShortSummary() + "\n");
+
+            StationWagon stationWagon1 = new StationWagon();
+            Console.WriteLine("stationWagon1 initial:\n" + stationWagon1.getShortSummary() + "\n");
         }
     }
-
-    class Car
+    
+    /* abstract base class containing only abstract methods: interface */
+    interface ICar
     {
-        // 1 private field
-        private String ownerId;
+        void toggleEngineStartButton();
+        void toggleDoorsLockButton();
+        void toggleHazardsButton();
+    }
 
-        // 2 public properties (auto-implemented property)
-        public String vinNumber { get; set; }
-        public String licensePlate { get; set; }
+    enum climateMode { manual, auto, off };
+
+    class Sedan : ICar
+    {
+        // private field(s)
+        private bool engineOn;
+        private bool doorsLocked;
+        private bool hazardsOn;
+        private double fuelLevel;
+        private climateMode climMode;
+
+        // public properties
+        public bool IsEngineRunning { get { return engineOn; } }
+        public bool AreDoorsLocked { get { return doorsLocked; } }
+        public bool IsHazardOn { get { return hazardsOn; } }
+        public double FuelTank { get { return fuelLevel; } set { fuelLevel = value; } }
 
         // 1 constructor
-        public Car(String owner)
+        public Sedan()
         {
-            this.ownerId = owner;
+            this.engineOn = false;
+            this.doorsLocked = true;
+            this.hazardsOn = false;
+            this.fuelLevel = 0;
+            this.climMode = climateMode.off;
         }
 
-        // 1 or more public method(s)
+        // interface implementation
+        public void toggleDoorsLockButton()
+        {
+            this.doorsLocked = !this.doorsLocked;
+        }
+
+        public void toggleEngineStartButton()
+        {
+            this.engineOn = !this.engineOn;
+        }
+
+        public void toggleHazardsButton()
+        {
+            this.hazardsOn = !this.hazardsOn;
+        }
+
+        // public method(s)
+        public void toggleClimateControlAutoButton()
+        {
+            if (this.climMode == climateMode.auto)
+            {
+                this.climMode = climateMode.off;
+            }
+            else
+            {
+                this.climMode = climateMode.auto;
+            }
+        }
+
         public String getShortSummary()
         {
-            return "Owner: " + this.ownerId + "\nVIN: " + this.vinNumber + "\nLicense plate: " + this.licensePlate;
+            return "Engine running?: " + this.IsEngineRunning + "\nDoors locked?: " + this.AreDoorsLocked + "\nHazard on?: " + this.IsHazardOn + "\nFuel level: " + this.FuelTank + "\nClimate mode: " + this.climMode;
         }
 
-        public void updateOwnerId(String newOwner)
+    }
+
+    class StationWagon : ICar
+    {
+        // private field(s)
+        private bool engineOn;
+        private bool doorsLocked;
+        private bool hazardsOn;
+        private double fuelLevel;
+        private bool rearHatchOpen;
+
+        // public properties
+        public bool IsEngineRunning { get { return engineOn; } }
+        public bool AreDoorsLocked { get { return doorsLocked; } }
+        public bool IsHazardOn { get { return hazardsOn; } }
+        public double FuelTank { get { return fuelLevel; } set { fuelLevel = value; } }
+
+        // 1 constructor
+        public StationWagon()
         {
-            this.ownerId = newOwner;
+            this.doorsLocked = true;
+            this.engineOn = false;
+            this.hazardsOn = false;
+            this.fuelLevel = 0;
+        }
+
+        // interface implementation
+        public void toggleDoorsLockButton()
+        {
+            this.doorsLocked = !this.doorsLocked;
+        }
+
+        public void toggleEngineStartButton()
+        {
+            this.engineOn = !this.engineOn;
+        }
+
+        public void toggleHazardsButton()
+        {
+            this.hazardsOn = !this.hazardsOn;
+        }
+
+        // public method(s)
+        public void toggleRearHatchOpenButton()
+        {
+            this.rearHatchOpen = true;
+        }
+
+        public String getShortSummary()
+        {
+            return "Engine running?: " + this.IsEngineRunning + "\nDoors locked?: " + this.AreDoorsLocked + "\nHazard on?: " + this.IsHazardOn + "\nFuel level: " + this.FuelTank + "\nRear Hatch open?: " + this.rearHatchOpen;
         }
 
     }
